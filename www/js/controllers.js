a6515f4409f1b5812e0deb3b952ba7977b1c5482
charlesfranca <charles-franca@live.com>
2017-01-31 15:14:13 -0200

    adicionando

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,26 +1,30 @@
 angular.module('app.controllers', [])
-  
+
 .controller('cameraTabDefaultPageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+    // You can include any angular dependencies as parameters for this function
+    // TIP: Access Route Parameters for your page via $stateParams.parameterName
+    function($scope, $stateParams) {
 
 
-}])
-   
-.controller('cartTabDefaultPageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+    }
+])
 
+.controller('cartTabDefaultPageCtrl', ['$scope', '$stateParams', 'LivroService', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+    // You can include any angular dependencies as parameters for this function
+    // TIP: Access Route Parameters for your page via $stateParams.parameterName
+    function($scope, $stateParams, LivroService) {
+        LivroService.getLivros(function(livros) {
+            console.log(livros);
+            $scope.livros = livros;
+        })
+    }
+])
 
-}])
-   
 .controller('cloudTabDefaultPageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+    // You can include any angular dependencies as parameters for this function
+    // TIP: Access Route Parameters for your page via $stateParams.parameterName
+    function($scope, $stateParams) {
 
 
-}])
-    
+    }
+])
--- www/js/services.js
+++ www/js/services.js
@@ -1,9 +1,50 @@
 angular.module('app.services', [])
 
-.factory('BlankFactory', [function(){
+.factory('BlankFactory', [function() {
 
 }])
 
-.service('BlankService', [function(){
+.service('LivroService', ['$http', function($http) {
+    var svc = {};
+    var livros = [{
+        title: "Livro 1",
+        imagem: "img/nemtaoprimos-livro.png",
+        precode: "90,00",
+        precopor: "50,00",
+        autor: "Charles França"
+    }, {
+        title: "Livro 2",
+        imagem: "img/nemtaoprimos-livro.png",
+        precode: "90,00",
+        precopor: "50,00",
+        autor: "Charles França"
+    }, {
+        title: "Livro 3",
+        imagem: "img/nemtaoprimos-livro.png",
+        precode: "90,00",
+        precopor: "50,00",
+        autor: "Charles França"
+    }];
 
+    svc.getLivros = function(callback) {
+        // Simple GET request example:
+        $http({
+            method: 'GET',
+            url: 'http://localhost:3000/livros'
+        }).then(function successCallback(resposta) {
+            callback(resposta.data);
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+    }
+
+    return svc;
 }]);
+
+
+// {
+//     config: {}, // Configurações utilizadas para fazer a solicitação
+//     data: {}, // Dados solicitados para a API
+//     headers: {} // Informações do header da solicitação
+// }
